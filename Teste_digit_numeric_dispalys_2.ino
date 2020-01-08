544ba2d4cc1310b276b974896ac4a1c77697c59c
Hedi Cherif <hedi.cherif@ictpro.labra>
2020-01-08 14:22:18 +0200

    Laitteisto todettu toimivaksi. Nelinumeroinen luku näytöllä

---
+++ Teste_digit_numeric_dispalys_2.ino
@@ -0,0 +1,196 @@
+//affiche le numero 1 et 2 et 3 et 4 en meme temp
+ #define SEGA 2
+ #define SEGB 6
+ #define SEGC 9
+ #define SEGD 11
+ #define SEGE 12
+ #define SEGF 3
+ #define SEGG 8
+ #define SEGDP 10
+
+#define DIG1 1
+#define DIG2 4
+#define DIG3 5
+#define DIG4 7
+ 
+#define SEGON LOW
+#define SEGOFF HIGH
+#define DIGON HIGH
+#define DIGOFF LOW 
+
+void setup() {
+  
+
+
+ 
+
+ 
+  pinMode(SEGA, OUTPUT);
+  pinMode(SEGB, OUTPUT);
+  pinMode(SEGC, OUTPUT);
+  pinMode(SEGD, OUTPUT);
+  pinMode(SEGE, OUTPUT);
+  pinMode(SEGF, OUTPUT);
+  pinMode(SEGG, OUTPUT);
+  pinMode(SEGDP, OUTPUT);
+  
+  pinMode(DIG1, OUTPUT);
+  pinMode(DIG2, OUTPUT);
+  pinMode(DIG3, OUTPUT);
+  pinMode(DIG4, OUTPUT);
+
+  
+  digitalWrite(SEGA, SEGON);
+
+} 
+void one() {
+  digitalWrite(SEGA, SEGOFF);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGOFF);
+  digitalWrite(SEGE, SEGOFF);
+  digitalWrite(SEGF, SEGOFF);
+  digitalWrite(SEGG, SEGOFF);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void two() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGOFF);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGON);
+  digitalWrite(SEGF, SEGOFF);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void three() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGOFF);
+  digitalWrite(SEGF, SEGOFF);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void four() {
+  digitalWrite(SEGA, SEGOFF);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGOFF);
+  digitalWrite(SEGE, SEGOFF);
+  digitalWrite(SEGF, SEGON);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void five() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGOFF);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGOFF);
+  digitalWrite(SEGF, SEGON);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void six() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGOFF);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGON);
+  digitalWrite(SEGF, SEGON);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void seven() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGOFF);
+  digitalWrite(SEGE, SEGOFF);
+  digitalWrite(SEGF, SEGOFF);
+  digitalWrite(SEGG, SEGOFF);
+  digitalWrite(SEGDP, SEGOFF);
+}
+
+void eight() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGON);
+  digitalWrite(SEGF, SEGON);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+void nine() {
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGOFF);
+  digitalWrite(SEGF, SEGON);
+  digitalWrite(SEGG, SEGON);
+  digitalWrite(SEGDP, SEGOFF);
+}
+
+void zero(){
+  digitalWrite(SEGA, SEGON);
+  digitalWrite(SEGB, SEGON);
+  digitalWrite(SEGC, SEGON);
+  digitalWrite(SEGD, SEGON);
+  digitalWrite(SEGE, SEGON);
+  digitalWrite(SEGF, SEGON);
+  digitalWrite(SEGG, SEGOFF);
+  digitalWrite(SEGDP, SEGOFF);
+}
+#define VIIVE 400
+#define UP 1
+#define DOWN -1
+int i=0;
+int dir=UP;
+
+void showNum (int x) {
+   switch(x){
+    case 0:zero();break;
+    case 1:one();break;
+    case 2:two();break;
+    case 3:three();break;
+    case 4:four();break;
+    case 5:five();break;
+    case 6:six();break;
+    case 7:seven();break;
+    case 8:eight();break;
+    case 9:nine();break;
+  }
+}
+
+int count=0;
+void loop(){
+  digitalWrite(DIG1, DIGON);
+  showNum(1);
+  delay (1);
+  digitalWrite(DIG1, DIGOFF);
+
+
+  digitalWrite(DIG2, DIGON);
+  showNum(2);
+  delay (1);
+  digitalWrite(DIG2, DIGOFF);
+
+    digitalWrite(DIG3, DIGON);
+  showNum(3);
+  delay (1);
+  digitalWrite(DIG3, DIGOFF);
+
+digitalWrite(DIG4, DIGON);
+  showNum(4);
+  delay (1);
+  digitalWrite(DIG4, DIGOFF);
+
+  
+ 
+
+  
+  }
